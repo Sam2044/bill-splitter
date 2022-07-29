6ecade348cd38608bee94995705a0d1902580eb3
Samuel Mzee <samuelmzeee40@gmail.com>
2022-07-28 20:13:45 -0500

    Add calculator service

---
+++ src/app/calculator.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculatorService {
+
+  TipAmountPerPerson="0.00";
+  TotalAmountPerPerson="0.00";
+
+}
