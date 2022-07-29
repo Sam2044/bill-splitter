ffac93bdaee5f89c32f9f19c33a8aa33a81e0a3f
Samuel Mzee <samuelmzeee40@gmail.com>
2022-07-28 20:15:07 -0500

    Add app template holding the two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TipResultsComponent } from './tip-results/tip-results.component';
+import { BillParametersComponent } from './bill-parameters/bill-parameters.component';
+import { CalculatorService } from './calculator.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipResultsComponent,
+    BillParametersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CalculatorService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
